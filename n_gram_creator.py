55eaa67b3e5a113c4dafd05e094f9332fd63ea1e
Sayan <sgdgp.2014@gmail.com>
2016-06-26 03:18:31 +0530

    Added json support to n_gram_creator.py

--- n_gram_creator.py
+++ n_gram_creator.py
@@ -1,6 +1,19 @@
+import json
+
 def ngrams(input, n):
-  input = input.split(' ')
-  output = []
-  for i in range(len(input)-n+1):
-    output.append(input[i:i+n])
-  return output
+	input = input.split(' ')
+  	output = []
+  	for i in range(len(input)-n+1):
+		output.append(input[i:i+n])
+  	return output
+	  
+	  
+def main():
+	fin_name = "cayasongi.txt"
+	fin = open(fin_name,'r')
+	out = {}
+	for row in fin:
+		row = row.strip()
+		out = ngrams(row,1)	
+	fout = open('n_gram_unigram.json'.'w')
+	json.dump(out,fout)	
