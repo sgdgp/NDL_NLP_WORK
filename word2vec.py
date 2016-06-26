235e5ca2047b0a62b2e4678ecac2f17f07f2835f
Sayan <sgdgp.2014@gmail.com>
2016-06-26 10:53:29 +0530

    Added 2nd draft and fixed similarity index for wor2vec.py

--- word2vec.py
+++ word2vec.py
@@ -12,14 +12,14 @@ class MySentences(object):
             yield line.split()
             
             
-def main():
+def word2vec_main():
     # sentences = MySentences('cayasongi.txt')
     # print sentences 
     s = "প্রতি বছর শীতের ছুটির সময় ভাবি কিছুদিন গ্রামে কাটিয়ে আসব। দলবল নিয়ে যাব- হৈচৈ করা যাবে। আমার বাচ্চারা কখনও গ্রাম দেখেনি- তারা খুশি হবে। পুকুরে ঝাঁপাঝাঁপি করতে পারবে। শাপলা ফুল শুধু যে মতিঝিলের সামনেই ফোটে না, অন্যান্য জায়গাতেও ফোটে তাও স্বচক্ষে দেখবে।"
     sent = []
     s = (s.strip()).split(' ')
     sent.append(s)
-    print sent
+    # print sent
     
     s2 = "আমার বেশির ভাগ পরিকল্পনাই শেষ পর্যন্ত কাজে লাগাতে পারি না। এটা কেমন করে জানি লেগে গেল। একদিন সত্যি সত্যি রওনা হলাম।"
     sent2 = []
@@ -27,12 +27,25 @@ def main():
     s2 = s2.split(' ')
     sent2.append(s2)
     
-    model = gensim.models.Word2Vec(sentences=s,min_count=1)
-    # model.build_vocab
-    model.train(sent2)
-    model.save('Word2VecModel.txt') 
-    print "done"
-    print model.similarity('আমার','বেশির')
+    model = gensim.models.word2vec.Word2Vec(sentences=sent,min_count=1)
+    print model.similarity('বছর','বছর')
     
+    
+def word2vec_test():
+    # testing 
+    s = "He is a man"
+    s = s.strip()
+    s = s.split()
+    s2 = []
+    s2.append(s)
+    print s
+    model = gensim.models.word2vec.Word2Vec(sentences=s2,min_count=1)
+    # model = gensim.models.Word2Vec(sentences=s,min_count=1)
+    print model.vocab
+    print model.similarity('man','man')
+    
+def main():
+    # word2vec_test()
+    word2vec_main()        
 if __name__=="__main__":
     main()               
